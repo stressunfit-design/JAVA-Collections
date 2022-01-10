7b0eefd50d3ab3040db8aeafb1f7abcbc4eb15c9
Srajan Poojary <74640796+stressunfit-design@users.noreply.github.com>
2022-01-10 21:01:03 +0530

    Delete Collection/ArrayList directory

--- Collection/ArrayList/ArrayListImplementation.java
+++
@@ -1,9 +0,0 @@
-class List{
-    public static void main(String[] args) {
-        
-        //  Implementing ArrayList :
-        // ArrayList<DataType> ArrayListName = new ArrayList<>();
-        
-        ArrayList<String> names = new ArrayList<>();
-    }
-}
