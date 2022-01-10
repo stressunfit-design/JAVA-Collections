79b3acbf5319caadcbc449dfaeae33497ad76e19
Srajan Poojary <74640796+stressunfit-design@users.noreply.github.com>
2022-01-10 20:57:51 +0530

    Create ArrayListImplementation.java

---
+++ Collection/ArrayList/ArrayListImplementation.java
@@ -0,0 +1,9 @@
+class List{
+    public static void main(String[] args) {
+        
+        //  Implementing ArrayList :
+        // ArrayList<DataType> ArrayListName = new ArrayList<>();
+        
+        ArrayList<String> names = new ArrayList<>();
+    }
+}
